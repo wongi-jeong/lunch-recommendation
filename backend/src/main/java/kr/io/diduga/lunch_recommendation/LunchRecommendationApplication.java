439608ad0ecd346ecb74fe58d7b367dbc1530dbe
h3136514 <3136514@naver.com>
2025-11-18 12:02:08 +0900

    feat: 초기셋팅(백엔드)

---
+++ backend/src/main/java/kr/io/diduga/lunch_recommendation/LunchRecommendationApplication.java
@@ -0,0 +1,13 @@
+package kr.io.diduga.lunch_recommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchRecommendationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunchRecommendationApplication.class, args);
+	}
+
+}
