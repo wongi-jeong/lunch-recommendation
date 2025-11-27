eaa852a12f988ff4e98e865c6ff4210158ee24f6
h3136514 <3136514@naver.com>
2025-11-27 11:19:29 +0900

    feat: 초기셋팅(프론트)

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<script setup>
+</script>
+
+<template>
+  <div class="greetings">
+    <h1>
+      You’ve successfully created a project with
+    </h1>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+</style>
